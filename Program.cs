d03e8e5fb30dbf0b08fc2694b720ad8035457cb9
Nouhou Ndach <nouhoundach@gmail.com>
2022-04-04 14:15:48 +0100

    Step1
    Implementation of navigation between different states!

--- Program.cs
+++ Program.cs
@@ -3,7 +3,39 @@
 namespace SimpleCalculator{
     class Program{
         static void Main(String[] args) {
-            Console.WriteLine("Hello, World!");
+            
+            int state = 0;
+            do{
+                switch (state) {
+                    case 0:
+                        Console.WriteLine("case 0");
+                        double operande1 = 0.0, operande2 = 0.0, result = 0.0;
+                        string operateur = "";
+                        state = 1;
+                        break;
+                    case 1:
+                        Console.WriteLine("case 1!");
+                        state = 2;
+                        break;
+                    case 2:
+                        Console.WriteLine("case 2!");
+                        state = 3;
+                        break;
+                    case 3:
+                        Console.WriteLine("case 3!");
+                        state = 4;
+                        break;
+                    case 4:
+                        Console.WriteLine("case 4!");
+                        state = 5;
+                        break;
+                    default:
+                        Console.WriteLine("default case");
+                        break;
+                }
+            } while(state != 5);
+
+            Console.WriteLine("Hello C#");
         }
     }
 }
