05b30d26c7dba1d00c507ede32f1a5d9cc2f7888
Nouhou Ndach <nouhoundach@gmail.com>
2022-04-04 15:43:42 +0100

    Step 2
    Getting and Parsing user inputs from standard input!

--- Program.cs
+++ Program.cs
@@ -8,16 +8,29 @@ namespace SimpleCalculator{
             do{
                 switch (state) {
                     case 0:
-                        Console.WriteLine("case 0");
+                        Console.WriteLine("Initialising Simple Calculator!");
                         double operande1 = 0.0, operande2 = 0.0, result = 0.0;
                         string operateur = "";
                         state = 1;
                         break;
-                    case 1:
-                        Console.WriteLine("case 1!");
+                    case 1: // Getting inputs
+                        Console.WriteLine("Getting user inputs!");
+                        
+                        Console.WriteLine("Enter first operand: ");
+                        string str1 = Console.ReadLine();
+                        if (!Double.TryParse(str1, out operande1)) {
+                            Console.WriteLine("Error: Enter a numerical value!");
+                            state = 5; // We stop the application.
+                        }
+                        Console.WriteLine("Enter second operand: ");
+                        string str2 = Console.ReadLine();
+                        if (!Double.TryParse(str2, out operande2)) {
+                            Console.WriteLine("Error: Enter a numerical value!");
+                            state = 5; // We stop the application.
+                        }
                         state = 2;
                         break;
-                    case 2:
+                    case 2: // Choose the operator to apply: + or - or * or /
                         Console.WriteLine("case 2!");
                         state = 3;
                         break;
